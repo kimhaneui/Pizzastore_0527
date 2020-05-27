9df0eb920d35ae9bb54aebdc429b53e9b3414338
kimhaneui <kimhaneui2@naver.com>
2020-05-27 13:15:09 +0900

    전화세팅 완료

--- app/src/main/java/com/example/pizzastore_0527/DetailActivity.java
+++ app/src/main/java/com/example/pizzastore_0527/DetailActivity.java
@@ -3,13 +3,25 @@ package com.example.pizzastore_0527;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
+import android.Manifest;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
 import com.example.pizzastore_0527.databinding.ActivityDetailBinding;
+import com.example.pizzastore_0527.datas.PizzaStore;
+import com.gun0912.tedpermission.PermissionListener;
+import com.gun0912.tedpermission.TedPermission;
+
+import java.util.List;
 
 public class DetailActivity extends BaseActivity {
 
     ActivityDetailBinding binding;
+    PizzaStore mStore;
 
 
     @Override
@@ -23,10 +35,48 @@ public class DetailActivity extends BaseActivity {
     @Override
     public void setupEvents() {
 
+        binding.callBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                PermissionListener pl = new PermissionListener() {
+                    @Override
+                    public void onPermissionGranted() {
+
+                        String phoneNum = binding.phoneNum.getText().toString();
+                        Uri myuri = Uri.parse(String.format("tel:%s",phoneNum));
+                        Intent myintent  = new Intent(Intent.ACTION_CALL,myuri);
+
+                        startActivity(myintent);
+
+                    }
+
+                    @Override
+                    public void onPermissionDenied(List<String> deniedPermissions) {
+
+                        Toast.makeText(mContext,"권한이 거부되어 통화가 불가능합니다",Toast.LENGTH_SHORT).show();
+
+                    }
+                };
+
+                TedPermission
+                        .with(mContext)
+                        .setPermissionListener(pl).
+                        setDeniedMessage("거부하면 통화가 불가능함.")
+                        .setPermissions(Manifest.permission.CALL_PHONE).check();
+            }
+        });
+
+
     }
 
     @Override
     public void setValues() {
+        mStore = (PizzaStore) getIntent().getSerializableExtra("store");
 
+        binding.storeName.setText(mStore.getName());
+        binding.phoneNum.setText(mStore.getPhoneNum());
+
+        Glide.with(mContext).load(mStore.getLogoImgUri()).into(binding.profileId);
     }
 }
